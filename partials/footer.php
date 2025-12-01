b81b4744d520dc97b55fbd0b5254e092d2ea6dc7
careyxos <154066489+careyxos@users.noreply.github.com>
2025-12-01 17:21:25 +0800

    Delete partials directory

--- partials/footer.php
+++
@@ -1,7 +0,0 @@
-    <footer class="site-footer">
-        <span>&copy; <?php echo date('Y'); ?> CampusConnect</span>
-        <span class="muted">Group 7 - BSIT IV - E</span>
-    </footer>
-</body>
-</html>
-
